c1aa13a822229456baa5dcda725e3af2718efad5
Srhiulli <srhiulli@gmail.com>
2023-05-16 21:54:09 -0300

    init project: fastify, typescript, prisma

---
+++ prisma/migrations/20230517004008_create_users_table/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL
+);
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/hello',async () => {
+    const users = await  prisma.user.findMany()
+    return users
+})
+
+app.listen({
+    port: 3333,
+}
+).then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+})
