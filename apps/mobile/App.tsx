7a304b4f5242a17c9773aad1e6227bd71f1a4450
Srhiulli <srhiulli@gmail.com>
2023-05-17 22:11:44 -0300

    updates

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className='text-zinc-50 font-bold text-5xl'>Hello world!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
---
+++ apps/nextjs-blog/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+   <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+    <h1 className="text-4xl font-bold">Sua Cápsula do tempo</h1>
+
+   </div>
+
+  )
+}
+
